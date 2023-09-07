b2be00fa93c9c1b7876205057479619c9f45fdd9
Glenn <Glenn@test.com>
2023-09-07 15:23:32 +0800

    Setup React bootstrap

--- app/App.js
+++ app/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Login from "./components/Login";
+import Footer from "./components/Footer";
 import UserManagement from "./components/UserManagement";
+// import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/user_management" element={<UserManagement />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
