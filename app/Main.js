73bc6fa2aa5d7c7d4a30c97f7af3208691d117c9
Glenn <Glenn@test.com>
2023-09-07 13:45:30 +0800

    first commit

---
+++ app/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <div>Hello World</div>;
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<App />);
