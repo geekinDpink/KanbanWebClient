20612a2605b4e6bb1af3020948d6073e20ae2763
Glenn <Glenn@test.com>
2023-09-07 17:37:58 +0800

    Formik UserDetailForm

--- app/App.js
+++ app/App.js
@@ -6,6 +6,7 @@ import Container from "react-bootstrap/Container";
 import Login from "./components/Login";
 import Footer from "./components/Footer";
 import UserManagement from "./components/UserManagement";
+import UserDetailForm from "./components/UserDetailForm";
 // import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
@@ -14,7 +15,7 @@ export default function App() {
       <Header />
       <Container style={{ marginTop: "20px" }}>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<UserDetailForm />} />
           <Route path="/user_management" element={<UserManagement />} />
         </Routes>
       </Container>
--- app/components/UserDetailForm.js
+++ app/components/UserDetailForm.js
@@ -1,13 +1,64 @@
 import React from "react";
-import Table from "react-bootstrap/Table";
-import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-export default function UserManagement() {
+import { Formik, Field, Form } from "formik";
+
+export default function UserDetailForm() {
   return (
     <>
-      <Row></Row>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <Container>
+            <Row style={{ marginTop: "8px", marginBottom: "8px" }}>
+              <Col xs md lg={1}>
+                <label htmlFor="username">Username</label>
+              </Col>
+              <Col xs md lg={4}>
+                <Field id="username" name="username" placeholder="Jane" />
+              </Col>
+            </Row>
+            <Row style={{ marginTop: "8px", marginBottom: "8px" }}>
+              <Col xs md={1}>
+                <label htmlFor="password">Password</label>
+              </Col>
+              <Col xs md={4}>
+                <Field id="password" name="password" placeholder="P@ssw0rd" />
+              </Col>
+            </Row>
+            <Row style={{ marginTop: "8px", marginBottom: "8px" }}>
+              <Col xs md={1}>
+                <label htmlFor="email">Email</label>
+              </Col>
+              <Col xs md={4}>
+                <Field id="email" name="email" placeholder="jane@hotmail.com" />
+              </Col>
+            </Row>
+            <Row style={{ marginTop: "8px", marginBottom: "8px" }}>
+              <Col xs md={1}>
+                <label htmlFor="usergroup">User Group</label>
+              </Col>
+              <Col xs md={4}>
+                <Field id="usergroup" name="usergroup" as="select" />
+              </Col>
+            </Row>
+            <Row style={{ marginTop: "5px", marginBottom: "5px" }}>
+              <button type="submit">Create</button>
+            </Row>
+          </Container>
+        </Form>
+      </Formik>
     </>
   );
 }
