e62e55439e919a6f673fd7e20856447921bf2d86
Glenn <Glenn@test.com>
2023-09-12 14:04:10 +0800

    Login: Toast for error handling

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,29 +2,41 @@ import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Toast from "react-bootstrap/Toast";
 
 export default function Header({ isLoggedIn, setIsLoggedIn }) {
+  const [showToast, setShowToast] = useState(false);
+  const [toastMsg, setToastMsg] = useState("");
   // Login: Correct - get token and store to local storage and Wrong - snack bar
   const submitLoginHandler = async (event) => {
     event.preventDefault(); // prevent form from resetting on submit
-    try {
-      const res = await axios.post("http://localhost:8080/login", {
-        username: event.target[0].value,
-        password: event.target[1].value,
-      });
-      if (res.data.token) {
-        // console.log("token", res.data);
-        localStorage.setItem("token", res.data.token);
-        setIsLoggedIn(true);
-      } else {
-        // console.log("no token", res.data);
+    if (event.target[0].value && event.target[1].value) {
+      try {
+        const res = await axios.post("http://localhost:8080/login", {
+          username: event.target[0].value,
+          password: event.target[1].value,
+        });
+        if (res.data.token) {
+          // console.log("token", res.data);
+          localStorage.setItem("token", res.data.token);
+          setIsLoggedIn(true);
+        } else {
+          setToastMsg(res.data.remarks);
+          setShowToast(true);
+
+          // console.log("no token", res.data);
+        }
+      } catch (err) {
+        console.log(err);
+        setToastMsg(err.response.data.remarks);
+        setShowToast(true);
       }
-    } catch (err) {
-      console.log(err);
+    } else {
+      setToastMsg("Incomplete/empty fields");
+      setShowToast(true);
     }
   };
 
@@ -44,6 +56,37 @@ export default function Header({ isLoggedIn, setIsLoggedIn }) {
           <Nav className="me-auto">
             <Nav.Link href="./">Home</Nav.Link>
             <Nav.Link href="./user_management">Manage Users</Nav.Link>
+
+            <Toast
+              style={{
+                zIndex: "999!important",
+                width: "200px",
+                height: "80px",
+              }}
+              delay={2000}
+              autohide
+              onClose={() => setShowToast(false)}
+              show={showToast}
+            >
+              <Toast.Header
+                style={{
+                  backgroundColor: "pink",
+                  color: "black",
+                  fontSize: "11px",
+                }}
+              >
+                Error: Unable to Login
+              </Toast.Header>
+              <Toast.Body
+                style={{
+                  backgroundColor: "pink",
+                  color: "black",
+                  fontSize: "11px",
+                }}
+              >
+                Reason: {toastMsg}
+              </Toast.Body>
+            </Toast>
             {!isLoggedIn ? (
               <Form
                 className="d-flex"
@@ -51,19 +94,19 @@ export default function Header({ isLoggedIn, setIsLoggedIn }) {
               >
                 <Form.Control
                   type="search"
-                  ide="username"
+                  id="username"
                   placeholder="Username"
                   className="me-2"
                   aria-label="username"
                 />
                 <Form.Control
                   type="search"
-                  ide="password"
+                  id="password"
                   placeholder="Password"
                   className="me-2"
                   aria-label="password"
                 />
-                <Button variant="outline-success" type="submit">
+                <Button variant="success" type="submit">
                   Login
                 </Button>
               </Form>
@@ -72,7 +115,7 @@ export default function Header({ isLoggedIn, setIsLoggedIn }) {
                 className="d-flex"
                 onSubmit={(event) => submitLogoutHandler(event)}
               >
-                <Button variant="outline-success" type="submit">
+                <Button variant="secondary" type="submit">
                   Logout
                 </Button>
               </Form>
