c694fce76a252a2a966f5546639c7f351b601361
Glenn <Glenn@test.com>
2023-09-13 16:57:22 +0800

    Fix Edit User redirect to other page when logout

--- app/App.js
+++ app/App.js
@@ -52,7 +52,7 @@ export default function App() {
               <Route
                 path="/"
                 element={
-                  isLoggedIn ? (
+                  state.isLoggedIn ? (
                     isAdmin ? (
                       <UserManagement />
                     ) : (
