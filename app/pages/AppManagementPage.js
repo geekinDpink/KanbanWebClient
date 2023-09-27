e34dcfa31a7459afd66ac956d44d734fe5bc9479
Glenn <Glenn@test.com>
2023-09-27 11:45:21 +0800

    Add table of app to app management page

--- app/pages/AppManagementPage.js
+++ app/pages/AppManagementPage.js
@@ -1,11 +1,19 @@
-import React, { useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import DispatchContext from "../../Context/DispatchContext";
 import StateContext from "../../Context/StateContext";
+import Table from "react-bootstrap/Table";
+import { Button } from "react-bootstrap";
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import axios from "axios";
+import { Container } from "react-bootstrap";
 
 export default function AppManagementPage() {
   const redDispatch = useContext(DispatchContext);
   const redState = useContext(StateContext);
+
+  const [applications, setApplications] = useState([]);
 
   // Authentication and Authorisation (Admin) Check
   useEffect(() => {
@@ -24,6 +32,14 @@ export default function AppManagementPage() {
           } else {
             redDispatch({ type: "notAdmin" });
           }
+
+          axios
+            .get("http://localhost:8080/apps", {
+              headers: { Authorization: `Basic ${token}` },
+            })
+            .then((response) => {
+              setApplications(response.data);
+            });
         })
         .catch((err) => {
           // api call is validation process e.g. token, if fail refuse entry and logout
@@ -36,9 +52,60 @@ export default function AppManagementPage() {
   }, []);
 
   return (
-    <>
-      <h1>AppManagementPage</h1>
-      <p>WIP</p>
-    </>
+    <Container>
+      <Row>
+        <Col xs s md={6}>
+          <h1>Application Management</h1>
+        </Col>
+        <Col xs s md={2}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              marginBottom: "5px",
+            }}
+          >
+            <Button onClick={() => navigate("/create_user")} variant="info">
+              Create App
+            </Button>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs s md={8}>
+          <Table bordered hover size="sm">
+            <thead>
+              <tr>
+                <th style={{ width: "5%" }}>Acronym</th>
+                <th style={{ width: "20%" }}>Description</th>
+                <th style={{ width: "5%" }}>Start Date</th>
+                <th style={{ width: "5%" }}>End Date</th>
+                <th style={{ width: "10%" }}>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {applications.length > 0 ? (
+                applications.map((app) => (
+                  <tr key={app.App_Acronym}>
+                    <td>{app.App_Acronym}</td>
+                    <td>{app.App_Description}</td>
+                    <td>{app.App_startDate}</td>
+                    <td>{app.App_endDate}</td>
+                    <td>
+                      <button>Edit App</button>
+                      <button>View Kanban Board</button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={5}>App Records are unavailable </td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
   );
 }
