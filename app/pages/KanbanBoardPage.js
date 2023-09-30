13f76c6e714cbb481b4b21fe20d2832c4badf1f5
geekinthepink <rawisglenn@gmail.com>
2023-09-30 19:51:42 +0800

    Kanban Board: Fetch Task and Rough UI

--- app/pages/KanbanBoardPage.js
+++ app/pages/KanbanBoardPage.js
@@ -1,75 +1,144 @@
-import React, { useContext, useEffect } from "react";
-import DispatchContext from "../../Context/DispatchContext";
+import React, { useContext, useState, useEffect } from "react";import DispatchContext from "../../Context/DispatchContext";
 import StateContext from "../../Context/StateContext";
+import { Col, Row, Container, ListGroup, Card } from "react-bootstrap";
 import axios from "axios";
-
 export default function KanbanBoardPage() {
   const redDispatch = useContext(DispatchContext);
   const redState = useContext(StateContext);
+
+  const [tasks, setTasks] = useState([]);
 
   // Authentication and Authorisation (Admin) Check
   useEffect(() => {
     const token = localStorage.getItem("token");
 
+    const authUserFetchData = async (token) => {
+      try {
+        const res = await axios.get("http://localhost:8080/user/admin", {
+          headers: { Authorization: `Basic ${token}` },
+        });
+
+        if (res.data.isAdmin) {
+          redDispatch({ type: "isAdmin" });
+        } else {
+          redDispatch({ type: "notAdmin" });
+        }
+      } catch (err) {
+        // api call is validation process e.g. token, if fail refuse entry and logout
+        console.log(err);
+        redDispatch({ type: "logout" });
+      }
+
+      try {
+        const response = await axios.get("http://localhost:8080/tasks", {
+          headers: { Authorization: `Basic ${token}` },
+        });
+        console.log("task res", response);
+        setTasks(response.data);
+      } catch (error) {
+        // api call is validation process e.g. token, if fail refuse entry and logout
+        console.log(error);
+        setApplications([]);
+      }
+    };
+
     if (token) {
       // Get my user detail based on username in token
-      axios
-        .get("http://localhost:8080/user/admin", {
-          headers: { Authorization: `Basic ${token}` },
-        })
-        .then((res) => {
-          console.log("Kanbanboard res", res.data);
-          if (res.data.isAdmin) {
-            redDispatch({ type: "isAdmin" });
-          } else {
-            redDispatch({ type: "notAdmin" });
-          }
-        })
-        .catch((err) => {
-          // api call is validation process e.g. token, if fail refuse entry and logout
-          console.log(err);
-          redDispatch({ type: "logout" });
-        });
+      authUserFetchData(token);
     } else {
       redDispatch({ type: "logout" });
     }
   }, []);
 
-  // Authentication Check
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token");
-
-  //   if (token) {
-  //     // Get my user detail based on username in token
-  //     axios
-  //       .get("http://localhost:8080/user", {
-  //         headers: { Authorization: `Basic ${token}` },
-  //       })
-  //       .then((res) => {
-  //         console.log("getUserProfile res", res.data);
-  //         // setUser(res.data[0]);
-  //         if (res) {
-  //           console.log("Kanban Before Disp logout", redState);
-  //           redDispatch({ type: "login" });
-  //           console.log("Kanban Before Disp logout", redState);
-  //         }
-  //       })
-  //       .catch((err) => {
-  //         // api call is validation process e.g. token, if fail refuse entry and logout
-  //         console.log(err);
-  //         console.log("CreateUser Before Disp logout", redState);
-  //         redDispatch({ type: "logout" });
-  //         console.log("CreateUser After Disp logout", redState);
-  //       });
-  //   } else {
-  //     redDispatch({ type: "logout" });
-  //   }
-  // }, []);
+  console.log(tasks);
 
   return (
     <>
-      <h1>KanbanBoard</h1>
-      <p>WIP</p>
+      <Container fluid>
+        <Row>
+          <Col xs={2}>
+            <Card>
+              <Card.Header>To Do</Card.Header>
+              <ListGroup>
+                {tasks
+                  .filter((task) => task.Task_state === "create")
+                  .map((task) => {
+                    console.log(task);
+                    return (
+                      <ListGroup.Item key={task.Task_id}>
+                        {task.Task_name}
+                      </ListGroup.Item>
+                    );
+                  })}
+              </ListGroup>
+            </Card>
+          </Col>
+          <Col xs={2}>
+            <Card>
+              <Card.Header>To Do List</Card.Header>
+              <ListGroup>
+                {tasks
+                  .filter((task) => task.Task_state === "todolist")
+                  .map((task) => {
+                    return (
+                      <ListGroup.Item key={task.Task_id}>
+                        {task.Task_name}
+                      </ListGroup.Item>
+                    );
+                  })}
+              </ListGroup>
+            </Card>
+          </Col>
+          <Col xs={2}>
+            <Card>
+              <Card.Header>Doing</Card.Header>
+              <ListGroup>
+                {tasks
+                  .filter((task) => task.Task_state === "doing")
+                  .map((task) => {
+                    return (
+                      <ListGroup.Item key={task.Task_id}>
+                        {task.Task_name}
+                      </ListGroup.Item>
+                    );
+                  })}
+              </ListGroup>
+            </Card>
+          </Col>
+          <Col xs={2}>
+            <Card>
+              <Card.Header>Done</Card.Header>
+              <ListGroup>
+                {tasks
+                  .filter((task) => task.Task_state === "done")
+                  .map((task) => {
+                    return (
+                      <ListGroup.Item key={task.Task_id}>
+                        {task.Task_name}
+                      </ListGroup.Item>
+                    );
+                  })}
+              </ListGroup>
+            </Card>
+          </Col>
+          <Col xs={2}>
+            <Card>
+              <Card.Header>Closed</Card.Header>
+              <ListGroup>
+                {tasks
+                  .filter((task) => task.Task_state === "closed")
+                  .map((task) => {
+                    return (
+                      <ListGroup.Item key={task.Task_id}>
+                        {task.Task_name}
+                      </ListGroup.Item>
+                    );
+                  })}
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
